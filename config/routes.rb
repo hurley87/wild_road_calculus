a4d4120c9ac0f1fa660ab1b5437dc0bb41ae9257
davidhurley <dhurls99@gmail.com>
2014-08-25 12:43:03 -0400

    add pruchase controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  
+
   root 'pages#home'
   get 'pages/about'
   resources :charges
+  resources :purchases, only: [:show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
